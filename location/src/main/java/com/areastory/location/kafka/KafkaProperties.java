ab32a09efa9850790fdd7e06164c9c1177995000
sumaee <1379psm@naver.com>
2024-11-06 21:00:12 +0900

    mysql -> postgreSql 변경

--- location/src/main/java/com/areastory/location/kafka/KafkaProperties.java
+++ location/src/main/java/com/areastory/location/kafka/KafkaProperties.java
@@ -3,7 +3,7 @@ package com.areastory.location.kafka;
 public interface KafkaProperties {
     String TOPIC_ARTICLE = "server-article";
     String GROUP_NAME_ARTICLE = "map-article";
-    String KAFKA_URL = "k8a3021.p.ssafy.io:9092";
+    String KAFKA_URL = "host.docker.internal:9092";
     String DELETE = "delete";
     String UPDATE = "update";
     String INSERT = "insert";
